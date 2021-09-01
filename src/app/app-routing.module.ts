549d3436deb3bc46e4727e204d495f2cc7f258fa
Lachartre2020 <s.lachartre@hotmail.fr>
2021-09-01 18:32:07 +0200

    quete router ok

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+
+const routes: Routes = [
+  { path: 'signup', component: SignUpComponent },
+  { path: 'user', component : UserProfileComponent},
+  { path: '**', component : UserProfileComponent},
+];
+
+export  {routes };
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UserComponent } from './user/user.component';
+import {routes} from './app-routing.module'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    SignUpComponent,
+    UserProfileComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
